a2d1a0301282976dc9e9c813ff29f5057b0e503e
satishmarutham <satishmarutham@gmail.com>
2016-11-05 18:50:02 +0530

    ChangeinSpringBootMain

--- src/main/java/com/control/SpringMvcApplication.java
+++ src/main/java/com/control/SpringMvcApplication.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 
@@ -13,9 +11,9 @@ public class SpringMvcApplication {
 public static HashMap<Long, Employee>	employee;
 
 public static void main(String[] args) {
-	
+		System.out.println("Main Method");
 		SpringApplication.run(SpringMvcApplication.class, args);
-		
+	
 	}
 }
 
